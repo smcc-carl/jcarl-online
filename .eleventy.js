d8084fe4c4d2a7a1a67e338afdf745e90e2492e3
smcc-carl <carl.smcc24@gmail.com>
2024-10-08 01:48:21 +0000

    add articles page and blogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
+// const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/main.js");
+
+  // let options = {
+	// 	html: true,
+	// 	breaks: true,
+	// 	linkify: true,
+	// };
+
+	// eleventyConfig.setLibrary("md", markdownIt(options));
+
 
 
   return{
